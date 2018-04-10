507a259ea582956ccd7dbfe3de91be5646bf9176
Kobee Raveendran <kobee.raveendran@knights.ucf.edu>
2018-04-10 12:07:47 -0400

    fixed most of the registration POST method

--- app/src/main/java/app/queuena/RequestHandler.java
+++ app/src/main/java/app/queuena/RequestHandler.java
@@ -1,4 +1,102 @@
 package app.queuena;
 
+import org.json.JSONObject;
+
+import java.io.BufferedOutputStream;
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.OutputStream;
+import java.io.OutputStreamWriter;
+import java.io.UnsupportedEncodingException;
+import java.net.HttpURLConnection;
+import java.net.URL;
+import java.net.URLEncoder;
+import java.util.HashMap;
+import java.util.Map;
+
+import javax.net.ssl.HttpsURLConnection;
+
 public class RequestHandler {
+
+
+
+    private String sendPostRequest(String requestURL, String[] params) {
+        URL url;
+
+        StringBuilder sb = new StringBuilder();
+
+        HttpURLConnection conn;
+
+        try {
+            url = new URL(requestURL);
+            JSONObject jsonobject = new JSONObject();
+            jsonobject.put("email", params[0]);
+            jsonobject.put("name", params[1]);
+            jsonobject.put("password", params[2]);
+            String message = jsonobject.toString();
+
+            conn = (HttpURLConnection) url.openConnection();
+            conn.setReadTimeout(10000);
+            conn.setConnectTimeout(15000);
+            conn.setRequestMethod("POST");
+            conn.setDoInput(true);
+            conn.setDoOutput(true);
+            conn.setRequestProperty("Content-Type", "application/json;charset=utf-8");
+
+            // open connection
+            conn.connect();
+
+            OutputStream os = new BufferedOutputStream(conn.getOutputStream());
+            os.write(message.getBytes());
+            os.flush();
+
+            InputStream is = conn.getInputStream();
+            /*
+            BufferedWriter writer = new BufferedWriter(new OutputStreamWriter(os, "UTF-8"));
+            writer.write(getPostDataString(postDataParams));
+
+            writer.flush();
+            writer.close();
+            os.close();
+
+            int responseCode = conn.getResponseCode();
+
+            if(responseCode == HttpsURLConnection.HTTP_OK) {
+                BufferedReader br = new BufferedReader(new InputStreamReader(conn.getInputStream()));
+
+                sb = new StringBuilder();
+
+                String response;
+                while((response = br.readLine()) != null) {
+                    sb.append(response);
+                }
+            }
+            */
+        }
+        catch(Exception e) {
+            e.printStackTrace();
+        }
+
+        return sb.toString();
+    }
+
+    private String getPostDataString(HashMap<String, String> params) throws UnsupportedEncodingException {
+        StringBuilder result = new StringBuilder();
+        boolean first = true;
+
+        for(Map.Entry<String, String> entry : params.entrySet()) {
+            if(first)
+                first = false;
+            else
+                result.append("&");
+
+            result.append(URLEncoder.encode(entry.getKey(), "UTF-8"));
+            result.append("=");
+            result.append(URLEncoder.encode(entry.getValue(), "UTF-8"));
+        }
+
+        return result.toString();
+    }
 }
