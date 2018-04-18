dec6a94c7b83324adf32b9a3bce85c88ecc58281
Nestor <nmontejo@knights,ucf.edu>
2018-04-18 03:43:22 -0400

    removed a large portion of unused code

--- app/src/main/java/app/queuena/SessionsActivity.java
+++ app/src/main/java/app/queuena/SessionsActivity.java
@@ -262,45 +262,4 @@ public class SessionsActivity extends AppCompatActivity {
         requestQueue.add(jsonRequest);
     }
 
-    /*
-    private void sessionIDFix(){
-        String url = "http://cop4331-2.com/API/SetSessionID.php";
-
-        RequestQueue requestQueue = Volley.newRequestQueue(SessionsActivity.this);
-
-        JSONObject payload = new JSONObject();
-
-
-        try {
-            payload.put("session", sessionGlobal.get(2));
-            payload.put("sessionID", sessionGlobal.get(3));
-            payload.put("sessionName", "");
-        } catch(JSONException e) {
-            e.printStackTrace();
-        }
-
-        JsonObjectRequest jsonRequest = new JsonObjectRequest(Request.Method.POST, url, payload, new Response.Listener<JSONObject>() {
-            @Override
-            public void onResponse(JSONObject response) {
-                try {
-                    String errorMsg = response.getString("error");
-
-                    if(!errorMsg.equals("")) {
-                        Toast.makeText(SessionsActivity.this, "set SessionID failure", Toast.LENGTH_SHORT).show();
-                    }
-
-                } catch (JSONException e) {
-                    e.printStackTrace();
-                }
-            }
-        }, new Response.ErrorListener() {
-            @Override
-            public void onErrorResponse(VolleyError error) {
-                VolleyLog.e("Error ", error.getMessage());
-            }
-        });
-
-        requestQueue.add(jsonRequest);
-    }
-    */
 }
